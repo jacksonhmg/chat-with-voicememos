a8bce8b9860825f43748accc916044464701fd00
jacksonhmg <jacksonmowattgok@gmail.com>
2023-10-31 16:11:38 -0700

    both files built. abt to test

--- langchain_helper.py
+++ langchain_helper.py
@@ -1,3 +1,5 @@
+# langchain_helper.py
+
 from langchain.document_loaders import YoutubeLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.llms import OpenAI
@@ -9,6 +11,8 @@ from dotenv import load_dotenv
 
 import os
 import whisper
+
+load_dotenv()
 
 embeddings = OpenAIEmbeddings()
 
@@ -23,4 +27,34 @@ def create_vector_db_from_memos(memo_files) -> FAISS:
 
         db = FAISS.from_documents(docs, embeddings)
         return db
-        # Do something with the embedding
+
+
+
+def get_response_from_query(db, query, openai_api_key, k=4):
+    # text-danvinci can handle 4097 tokens
+
+    docs = db.similarity_search(query, k=k)
+    docs_page_content = " ".join([d.page_content for d in docs])
+
+    llm = OpenAI(model="gpt-3.5-turbo", openai_api_key=openai_api_key)
+
+    prompt = PromptTemplate(
+        input_variables=["question", "docs"],
+        template="""
+        You are a helpful assistant that that can answer questions about a user's transcribed voice memos. 
+        
+        Answer the following question: {question}
+        By searching the following memos: {docs}
+                
+        If you feel like you don't have enough information to answer the question, say "I don't know".
+        
+        Your answers should be verbose and detailed.
+        """,
+    )
+
+    chain = LLMChain(llm=llm, prompt=prompt)
+
+    response = chain.run(question=query, docs=docs_page_content)
+    response = response.replace("\n", "")
+
+    return response
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+# main.py
+
 import streamlit as st
 import langchain_helper as lch
 import textwrap
 
-st.title("Youtube Assistant")
+st.title("Chat With Voice Memos")
 
 with st.sidebar:
     with st.form(key='my_form'):
@@ -15,12 +17,23 @@ with st.sidebar:
             label="Upload all your memo files",
             accept_multiple_files=True,
         )
-
+        query = st.sidebar.text_area(
+            label="Ask a question about your memos",
+            max_chars=300,
+            key="query",
+        )
         submit_button = st.form_submit_button(label='Submit')
 
-if memo_files and api_key:
-    db = lch.create_vector_db_from_memos(memo_files)
-    response = lch.get_response_from_query(db, query, api_key)
+# Check if the database is already created in the session state
+if 'db' not in st.session_state:
+    st.session_state.db = None
+
+# Create the database only if memo files are uploaded and the database does not exist
+if memo_files and api_key and not st.session_state.db:
+    st.session_state.db = lch.create_vector_db_from_memos(memo_files)
+
+# Query submission and response
+if query and st.session_state.db:
+    response = lch.get_response_from_query(st.session_state.db, query, api_key)
     st.subheader("Answer:")
     st.text(textwrap.fill(response, 80))
-        
